e2a2d4f599c8e525d5c746896340c0c9cc6f9106
c0d3m8kr <c0d3m8kr@gmail.com>
2022-04-11 09:04:03 -0700

    Added tictactoe cpp file

---
+++ project_tictactoe/tictactoe.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <array>
+
+using namespace std;
+
+// declare function prototypes
+void runGame();     // starts the game in main
+
+
+int main()
+{
+
+    runGame();
+
+    return 0;
+}
+
+void runGame()
+{
+    // initiate the functions here to execute the game
+    cout << "hello, world" << endl;
+}
