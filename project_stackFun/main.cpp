da8ce4461ec92b49e2a4424ced3d268ef505729e
c0d3m8kr <c0d3m8kr@gmail.com>
2022-04-27 23:10:21 -0400

    added stackFun

---
+++ project_stackFun/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+void storeReverse(string originalString, stack<char>& reverseStack);
+
+int main()
+{
+
+
+    return 0;
+}
+
+void storeReverse(string originalString, stack<char>& reverseStack)
+{
+    
+}
