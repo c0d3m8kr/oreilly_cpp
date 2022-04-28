ec303a9b81ca3d4a0d8960bd805b0d5b6562cb9d
c0d3m8kr <c0d3m8kr@gmail.com>
2022-04-27 23:40:44 -0400

    completed stackFun

--- project_stackFun/main.cpp
+++ project_stackFun/main.cpp
@@ -4,16 +4,60 @@
 
 using namespace std;
 
-void storeReverse(string originalString, stack<char>& reverseStack);
+void storeReverse(string origString, stack<char>& reverseStack);
+bool isPalindrome(string origString);
+void printResult(string origString);
 
 int main()
 {
+    string word;
+    cout << "Please enter a word: ";
+    cin >> word;
 
+    printResult(word);
 
     return 0;
 }
 
-void storeReverse(string originalString, stack<char>& reverseStack)
+void storeReverse(string origString, stack<char>& reverseStack)
 {
-    
+    for (char c : origString)
+    {
+        reverseStack.push(c);
+    }
+}
+
+bool isPalindrome(string origString)
+{
+    stack<char> reverseStack;
+    storeReverse(origString, reverseStack);
+
+    bool result = true;
+
+    if (origString.length() == reverseStack.size())
+    {
+        for (char c : origString)
+        {
+            if (c != reverseStack.top())
+            {
+                result = false;
+                break;
+            }
+            reverseStack.pop();
+        }
+    }
+    else
+    {
+        result = false;
+    }
+
+    return result;
+}
+
+void printResult(string origString)
+{
+    if (isPalindrome(origString))
+        cout << origString << " is a palindrome." << endl;
+    else
+        cout << origString << " is not a palindrome." << endl;
 }
